ced858a8a729f2a34e7af05e2cdae2e377364259
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-03-25 10:24:22 -0400

    added tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-4xl font-bold underline">Vite + React</h1>
+      <h1 className="text-4xl font-bold underline">
+      Hello world!
+    </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
