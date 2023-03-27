1f1f2966e1098b123133def7c854e0b5f1f77020
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-03-26 21:34:02 -0400

    added more packages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import './styles/tailwind.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import "./styles/tailwind.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
