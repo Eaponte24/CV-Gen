6a321a2063ee617496ae86fc503c2265b50ae501
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-03-27 23:17:28 -0400

    added more stlying just to test tailwin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50">
+      <div className='relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,12 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-4xl font-bold underline">Vite + React</h1>
-      <h1 className="text-4xl font-bold underline">
+      <h1 className="text-4xl font-bold underline  text-gray-900">Vite + React</h1>
+      <h1 className="text-4xl font-bold underline  text-gray-900">
       Hello world!
     </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="mx-auto max-w-md">
+        <button className='font-bold text-2xl text-slate-900 ring-1 ring-black p-1 my-2 shadow-xl ' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
