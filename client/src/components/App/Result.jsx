5ff4a123aaf3be8ef4e0f4197de8cad04e806ea3
theLemmonade <lemmonmail@icloud.com>
2023-04-13 13:30:04 -0400

    basic result page with copy and regen button

---
+++ client/src/components/App/Result.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const Result = ({ combinedInput, onRegenerate }) => {
+  const [resultText, setResultText] = useState(combinedInput);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(resultText).then(
+      () => {
+        alert('Text copied to clipboard!');
+      },
+      (err) => {
+        alert('Failed to copy text. Please try again.');
+      }
+    );
+  };
+
+  const handleRegenerate = () => {
+    onRegenerate();
+  };
+
+  const handleChange = (e) => {
+    setResultText(e.target.value);
+  };
+
+  return (
+    <div className="resultDiv">
+      <textarea
+        className="resultText"
+        value={resultText}
+        onChange={handleChange}
+      />
+      <div className="resultBtnDiv">
+        <button className="copyBtn" onClick={handleCopy}>
+          Copy
+        </button>
+        <button className="regenBtn" onClick={handleRegenerate}>
+          Regenerate
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Result;
