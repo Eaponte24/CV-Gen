312e251836b60dd17e1647ee33efaa1b7fe2c0c8
theLemmonade <lemmonmail@icloud.com>
2023-04-13 13:35:22 -0400

    updated questionnaire.jsx

--- client/src/components/App/Questonnaire.jsx
+++ client/src/components/App/Questonnaire.jsx
@@ -3,6 +3,7 @@ import "../../App.css";
 import Adjective from "./Questionnaire/Adjective";
 import Experience from "./Questionnaire/Experience";
 import Listing from "./Questionnaire/Listing";
+import Result from "../Result";
 
 const QuestionApp = () => {
 	const [currentStep, setCurrentStep] = useState(1);
@@ -13,16 +14,22 @@ const QuestionApp = () => {
 		setCurrentStep((prevStep) => prevStep + 1);
 	};
 
-	const handleSubmit = () => {
-		console.log("Combined input:", combinedInput.trim());
-		// Here, you can make the API call to ChatGPT with the combined input
-	};
+  const handleResultSubmit = () => {
+    console.log('Combined input:', combinedInput.trim());
+    // Here, you can make the API call to ChatGPT with the combined input
+    setCurrentStep(4);
+  };
+
+  const handleRegenerate = () => {
+    // Make the API call to ChatGPT with the combined input again, and update the result
+  };
 
 	return (
 		<div className="App">
 			{currentStep === 1 && <Adjective onSubmit={handleUserInputSubmit} />}
 			{currentStep === 2 && <Experience onSubmit={handleUserInputSubmit} />}
-			{currentStep === 3 && <Listing onSubmit={handleSubmit} />}
+			{currentStep === 3 && <Listing onSubmit={handleResultSubmit} />}
+			{currentStep === 4 && (<Result combinedInput={combinedInput} onRegenerate={handleRegenerate} />)}
 		</div>
 	);
 };
