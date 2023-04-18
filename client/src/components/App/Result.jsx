c1db0abb9dd0a5c73a76cc8bf9548aacfdb3e85b
theLemmonade <lemmonmail@icloud.com>
2023-04-18 16:58:39 -0400

    major style edits

--- client/src/components/App/Result.jsx
+++ client/src/components/App/Result.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-// import DownloadButton from "../DownloadButton";
 import changeBackgroundColor from "../../utils/changeBackgroundColor";
+import { ClipboardDocumentIcon, ClipboardDocumentCheckIcon } from "@heroicons/react/24/outline";
+// import DownloadButton from "../DownloadButton";
 
 const resultMessages = [
 	"Good luck.",
@@ -13,31 +14,23 @@ const resultMessages = [
 	"You can do this.",
 	"Here's your cover letter.",
 	"Don't tell them how you came up with this.",
-  ];
+];
 
 function getRandomMessage() {
 	const randomIndex = Math.floor(Math.random() * resultMessages.length);
 	return resultMessages[randomIndex];
-  }
+}
 
 const Result = ({ combinedInput, onRegenerate }) => {
 	const [resultText, setResultText] = useState(combinedInput);
 	const [resultHeader, setResultHeader] = useState(getRandomMessage());
+	const [copyIcon, setCopyIcon] = useState(
+		<ClipboardDocumentIcon className="h-5 w-5" />
+	);
 
 	useEffect(() => {
 		changeBackgroundColor();
 	}, []);
-
-	const handleCopy = () => {
-		navigator.clipboard.writeText(resultText).then(
-			() => {
-				alert("Text copied to clipboard!");
-			},
-			(err) => {
-				alert("Failed to copy text. Please try again.");
-			}
-		);
-	};
 
 	const handleRegenerate = () => {
 		onRegenerate();
@@ -45,6 +38,25 @@ const Result = ({ combinedInput, onRegenerate }) => {
 
 	const handleChange = (e) => {
 		setResultText(e.target.value);
+	};
+
+	useEffect(() => {
+		changeBackgroundColor();
+	}, []);
+
+	const handleCopy = async () => {
+		try {
+			await navigator.clipboard.writeText(resultText);
+			console.log("Copied to clipboard")
+			// Change the icon to the ClipboardCheckIcon
+			setCopyIcon(<ClipboardDocumentCheckIcon className="h-5 w-5" />);
+			// Change the icon back to the ClipboardIcon after a short delay
+			setTimeout(() => {
+				setCopyIcon(<ClipboardDocumentIcon className="h-5 w-5" />);
+			}, 1000);
+		} catch (err) {
+			console.error("Failed to copy text: ", err);
+		}
 	};
 
 	const autoResize = (event) => {
@@ -58,7 +70,9 @@ const Result = ({ combinedInput, onRegenerate }) => {
 			<p
 				id="resultHeader"
 				className="text-white-900 my-10 text-3xl font-bold tracking-tight sm:text-4xl"
-			>{resultHeader}</p>
+			>
+				{resultHeader}
+			</p>
 			<textarea
 				id="resultText"
 				value={resultText}
@@ -73,21 +87,7 @@ const Result = ({ combinedInput, onRegenerate }) => {
 					className="resultBtn mr-3 rounded-md bg-white px-2.5 py-1.5 text-lg font-bold text-gray-900 shadow-sm ring-1 hover:bg-gray-200"
 					onClick={handleCopy}
 				>
-					Copy{" "}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth={1.5}
-						stroke="currentColor"
-						className="h-6 w-6"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
-						/>
-					</svg>
+					Copy {copyIcon}
 				</button>
 				{/* <DownloadButton /> goes here later*/}
 				<button
