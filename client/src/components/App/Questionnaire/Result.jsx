7eaa4adeeec2657c6c0d6aa867de98afbe4a6f38
theLemmonade <lemmonmail@icloud.com>
2023-04-24 14:39:13 -0400

    moved result to new folder

--- client/src/components/App/Questionnaire/Result.jsx
+++ client/src/components/App/Questionnaire/Result.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import changeBackgroundColor from "../../utils/changeBackgroundColor";
+import changeBackgroundColor from "../../../utils/changeBackgroundColor";
 import {
 	ClipboardDocumentIcon,
 	ClipboardDocumentCheckIcon,
