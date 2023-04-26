615e9ab37435a76d587c9531f5a8c6b7ecfa1bed
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-04-26 15:16:45 -0400

    added palceholder text so we dont run the api everytime

--- client/src/components/App/Questonnaire.jsx
+++ client/src/components/App/Questonnaire.jsx
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { useMutation } from "@apollo/client";
+// import { useMutation } from "@apollo/client";
 import Adjective from "./Questionnaire/Adjective";
 import Experience from "./Questionnaire/Experience";
 import Listing from "./Questionnaire/Listing";
 import Result from "./Questionnaire/Result";
 import Loading from "./Questionnaire/Loading";
-import { GEN_RESPONSE } from "../.././utils/mutation";
+// import { GEN_RESPONSE } from "../.././utils/mutation";
 
 const QuestionApp = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [combinedInput, setCombinedInput] = useState("");
   const [generatedResponse, setGeneratedResponse] = useState("");
-  const [generateResponse, { loading, error, data }] =
-    useMutation(GEN_RESPONSE);
+  // const [generateResponse, { loading, error, data }] =
+  //   useMutation(GEN_RESPONSE);
+  // this is just here for testing purposes
+  const placeholderResponse = "Dear [Employer],\n\nI am writing to apply for the position of [Job Title] at [Company]. I am confident that my skills and experience make me a strong candidate for the role. In my current position at [Current Company], I have gained valuable experience in [relevant skills].\n\nThank you for considering my application. I look forward to the opportunity to further discuss my qualifications with you.\n\nSincerely,\n[Your Name]";
 
   useEffect(() => {
-    if (data) {
-      console.log("data: ", data);
-      setGeneratedResponse(data.generateResponse);
-    }
-  }, [data]);
+    // if (data) {
+    //   console.log("data: ", data);
+    //   setGeneratedResponse(data.generateResponse);
+    // }
+    setGeneratedResponse(placeholderResponse);
+  }, []);
 
   const handleUserInputSubmit = (userInput) => {
     // collect, format, and store the user input
@@ -31,17 +34,18 @@ const QuestionApp = () => {
     const updatedInput = `${combinedInput} ${userInput}`;
     handleUserInputSubmit(userInput);
     console.log("Combined Input = ", updatedInput);
-    try {
-      await generateResponse({
-        variables: {
-          prompt: updatedInput,
-          model: "text-davinci-003",
-          max_tokens: 1100,
-        },
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    // try {
+    //   await generateResponse({
+    //     variables: {
+    //       prompt: updatedInput,
+    //       model: "text-davinci-003",
+    //       max_tokens: 1100,
+    //     },
+    //   });
+    // } catch (error) {
+    //   console.error(error);
+    // }
+    setGeneratedResponse(placeholderResponse);
   };
 
   const handleRegenerate = async () => {
@@ -53,9 +57,11 @@ const QuestionApp = () => {
       {currentStep === 1 && <Adjective onSubmit={handleUserInputSubmit} />}
       {currentStep === 2 && <Experience onSubmit={handleUserInputSubmit} />}
       {currentStep === 3 && <Listing onSubmit={handleResultSubmit} />}
-      {currentStep === 4 && (<Result generatedResponse={generatedResponse} onRegenerate={handleRegenerate} />)}
-      {loading && <Loading/>}
-      {error && <p>Error: {error.message}</p>}
+      {currentStep === 4 && (
+        <Result generatedResponse={generatedResponse} onRegenerate={handleRegenerate} />
+      )}
+      {/* {loading && <Loading />} */}
+      {/* {error && <p>Error: {error.message}</p>} */}
     </div>
   );
 };
