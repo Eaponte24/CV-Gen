8bf05b42e89dea36b73dcb574e1ee5cfcf638d64
theLemmonade <lemmonmail@icloud.com>
2023-04-27 18:19:16 -0400

    finished navbar v1

--- client/src/components/App/Header/Nav.jsx
+++ client/src/components/App/Header/Nav.jsx
@@ -27,23 +27,23 @@ export default function Header() {
 						<a
 							key={item.name}
 							href={item.href}
-							className="text-wh-900 text-sm font-semibold leading-6"
+							className="text-wh-900 text-lg font-semibold leading-6"
 						>
 							{item.name}
 						</a>
 					))}
-					<span id="soonBadge">Coming soon</span>
+					<span id="soonBadge">Coming soon!</span>
 				</div>
 				<div className="flex flex-1 items-center justify-end gap-x-6">
 					<a
 						href="#"
-						className="lg:text-white-900 hidden lg:block lg:text-sm lg:font-semibold lg:leading-6"
+						className="lg:text-white-900 hidden lg:block lg:text-lg lg:font-semibold lg:leading-6"
 					>
 						Log in
 					</a>
 					<a
 						href="#"
-						className="rounded-md bg-white px-3 py-2 px-2.5 py-1.5 text-sm text-lg font-semibold font-bold text-gray-900 shadow-sm ring-1 hover:bg-gray-200"
+						className="rounded-md bg-white px-3 py-2 px-2.5 py-1.5 text-lg font-semibold text-gray-900 shadow-sm ring-1 hover:bg-gray-200"
 					>
 						Support us
 					</a>
@@ -72,13 +72,13 @@ export default function Header() {
 							<span className="sr-only">Your Company</span>
 							<img
 								className="h-8 w-auto"
-								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								src="icon goes here"
 								alt=""
 							/>
 						</a>
 						<a
 							href="#"
-							className="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							className="ml-auto rounded-md bg-green-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
 						>
 							Support Us
 						</a>
@@ -98,7 +98,7 @@ export default function Header() {
 									<a
 										key={item.name}
 										href={item.href}
-										className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+										className="-mx-3 block rounded-lg px-3 py-2 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 									>
 										{item.name}
 									</a>
@@ -107,7 +107,7 @@ export default function Header() {
 							<div className="py-6">
 								<a
 									href="#"
-									className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+									className="-mx-3 block rounded-lg px-3 py-2.5 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>
 									Log in
 								</a>
