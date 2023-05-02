43d9c6ba8070564d8c1e9d1382c95864576f449a
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-05-02 10:13:16 -0400

    added the pricing routing

--- client/src/components/App/Header/Nav.jsx
+++ client/src/components/App/Header/Nav.jsx
@@ -38,7 +38,7 @@ export default function Header() {
         <div className="flex flex-1 items-center justify-end gap-x-6">
           <Link
             to="/login"
-            className="-mx-3 block rounded-lg px-3 py-2.5 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+            className="-mx-3 block rounded-lg px-3 py-2.5 text-lg font-semibold leading-7 text-white-900 underline"
           >
             Log in
           </Link>
@@ -73,12 +73,12 @@ export default function Header() {
               <span className="sr-only">Your Company</span>
               <img className="h-8 w-auto" src="icon goes here" alt="" />
             </a>
-            <a
-              href="#"
-              className="ml-auto rounded-md bg-green-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+            <Link
+              to="/pricing"
+              className="rounded-md bg-white px-3 py-2 px-2.5 py-1.5 text-lg font-semibold text-gray-900 shadow-sm ring-1 hover:bg-gray-200"
             >
-              Support Us
-            </a>
+              Support us
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -100,14 +100,6 @@ export default function Header() {
                     {item.name}
                   </a>
                 ))}
-              </div>
-              <div className="py-6">
-                <Link
-                  to="/login"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </Link>
               </div>
             </div>
           </div>
