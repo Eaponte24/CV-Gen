148029d746e657a0592d780e94e5f5346715705e
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-05-04 20:08:17 -0400

    added frontend mutation and queriews

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -6,3 +6,23 @@ export const GEN_RESPONSE = gql`
   }
 `;
 
+export const ADD_USER = gql`
+  mutation addUser($email: String!, $password: String!) {
+    addUser(email: $email, password: $password) {
+      email
+      password
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        email
+        password
+    }
+  }
+`;
+
+
+
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,14 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me {
+        me {
+            _id
+            email
+            password
+        }
+    }
+`;
 
 
 
